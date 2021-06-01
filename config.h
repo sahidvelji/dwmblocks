7ad076f91add99e8d79fa0724fc2096c98c83ffb
Sahid Velji <sahidvelji@gmail.com>
2021-06-01 15:41:31 -0400

    Add pomodoro timer

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-pomodoro",	10,	2},
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"sb-tasks",	10,	26}, */
